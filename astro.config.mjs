5f859f3b947b8f0d53f4003a6b150131abe8acc6
Chinmoy <daschinmoyy21@gmail.com>
2025-12-17 01:53:55 +0530

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import fulldevUi from 'fulldev-ui';
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [fulldevUi()]
+});
